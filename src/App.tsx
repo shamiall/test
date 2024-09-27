7b4924d101d6d4bbf5720182638063cac14d2d2d
shamiall <abdulabdulika@gmail.com>
2024-09-27 16:07:25 +0500

    fds

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useAccount } from 'wagmi'
+import { usePrivy } from '@privy-io/react-auth'
+
+type buttonProps = {
+  cta: string;
+  onClick_: () => void;
+  disabled?: boolean;
+};
+
+const Button = ({cta, onClick_, disabled}: buttonProps) => {
+  return (
+    <button
+      className="rounded bg-slate-800 px-10 py-2 text-white transition-all hover:bg-slate-900 active:bg-slate-900 enabled:hover:cursor-pointer enabled:active:scale-90 disabled:opacity-80"
+      onClick={onClick_}
+      disabled={disabled}
+    >
+      {cta}
+    </button>
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0)
+  const {address} = useAccount();
+  const {connectWallet} = usePrivy();
+
 
   return (
     <>
@@ -16,6 +39,9 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+      <p>Wallet address: {address}</p>
+      <Button onClick_={connectWallet} cta="Connect only" />
+
       {/* <h1>Vite + React</h1> */}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {PrivyClientConfig, PrivyProvider} from '@privy-io/react-auth';
+import {WagmiProvider} from '@privy-io/wagmi';
+import App from './App';
+import { config } from './wagmiConfig';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { createRoot } from 'react-dom/client';
+
+const privyConfig: PrivyClientConfig = {
+  embeddedWallets: {
+    createOnLogin: 'users-without-wallets',
+    requireUserPasswordOnCreate: true,
+    noPromptOnSignature: false,
+  },
+  loginMethods: ['wallet', 'email', 'sms'],
+  appearance: {
+    showWalletLoginFirst: true,
+  },
+};
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+<PrivyProvider appId='cm1kltqtz079lr962cs0mn7hs' config={privyConfig}>
+  <QueryClientProvider client={queryClient}>
+    <WagmiProvider config={config}>
+      <App />
+    </WagmiProvider>
+  </QueryClientProvider>
+</PrivyProvider>
 )
